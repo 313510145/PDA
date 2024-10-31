e5e072c3471a9b75e38ac9d65e920d568ff46c28
313510145 <cehong.ee13@nycu.edu.tw>
2024-10-31 15:13:29 +0800

    Lab2: delete the object if any file cannot be opened

--- Lab2-fixed_outline_floorplanner/main.cpp
+++ Lab2-fixed_outline_floorplanner/main.cpp
@@ -6,17 +6,18 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <alpha_value> <input_block> <input_net> <output_file>\n";
         exit(-1);
     }
-    fixed_outline_floorplanner* fof = new fixed_outline_floorplanner(argv[1]);
     std::ifstream input_file(argv[2]);
     if (!input_file) {
         std::cout << "Cannot open the input file: " << argv[2] << std::endl;
         exit(-1);
     }
+    fixed_outline_floorplanner* fof = new fixed_outline_floorplanner(argv[1]);
     fof->input_block(input_file);
     input_file.close();
     input_file.open(argv[3]);
     if (!input_file) {
         std::cout << "Cannot open the input file: " << argv[3] << std::endl;
+        delete fof;
         exit(-1);
     }
     fof->input_net(input_file);
